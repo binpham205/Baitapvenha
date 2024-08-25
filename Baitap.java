b2d17f7d39c5b8116eafe5752bd7131aca6f9647
Binpham205 <nhattanpham2005@gmail.com>
2024-08-25 11:10:05 +0700

    init project

---
+++ Baitap.java
@@ -0,0 +1,22 @@
+
+package baitap;
+import java.util.Scanner;
+
+public class Baitap {
+
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("a+b=" + (a+b));
+        System.out.println("a-b=" + (a-b));
+        System.out.println("a*b=" + (a*b) );
+        System.out.println("a/b=" + (a/b));
+        System.out.println("a%b=" + (a%b));
+        
+    }
+    
+        
+    }
+    
